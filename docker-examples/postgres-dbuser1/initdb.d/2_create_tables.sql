bf10f447908dc14128a25e4a025380d5adda1957
unmyr <56982541+unmyr@users.noreply.github.com>
2022-09-11 18:38:58 +0900

    refactor(docker-examples): Improve sql and Makefile.

--- docker-examples/postgres-dbuser1/initdb.d/2_create_tables.sql
+++ docker-examples/postgres-dbuser1/initdb.d/2_create_tables.sql
@@ -1,12 +1,7 @@
--- DROP SCHEMA IF EXISTS dbuser1 CASCADE;
 \connect fruits;
 \conninfo
 
 SET SCHEMA 'dbuser1';
-\conninfo
-SELECT current_database();
-
-DROP TABLE IF EXISTS fruits_menu CASCADE;
 
 CREATE TABLE fruits_menu (
   id SERIAL PRIMARY KEY,
@@ -18,10 +13,10 @@ CREATE TABLE fruits_menu (
 GRANT SELECT,INSERT,UPDATE,DELETE ON fruits_menu TO dbuser1;
 GRANT USAGE ON SEQUENCE fruits_menu_id_seq TO dbuser1;
 
-ALTER USER dbuser1 SET search_path TO 'dbuser1, pg_catalog';
-\l
-\d fruits_menu
-\dn+
-
 INSERT INTO fruits_menu (name, price) VALUES
   ('Apple', 100), ('Banana', 120), ('Orange', 110);
+
+\l
+\dt dbuser1.*
+\d fruits_menu
+\dn+
