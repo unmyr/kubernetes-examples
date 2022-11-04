c9fb14e3abc218a06463d8ccbffc4daff288aa81
unmyr <56982541+unmyr@users.noreply.github.com>
2022-11-05 00:24:40 +0900

    feat(greet-node): Add a Node.js example.

---
+++ greet-apps/greet-node/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const log4js = require('log4js');
+
+const logger = log4js.getLogger();
+logger.level = 'debug';
+
+const port = 8080;
+
+const app = express();
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.get('/api/greet/:name', (req, res) => {
+  logger.debug(`GET: /api/greep/${req.params.name}}`);
+  res.json({ message: `Hello ${req.params.name}!` });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+module.exports = app;
