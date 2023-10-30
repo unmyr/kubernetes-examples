559e4b420b01713d3275cd3677c7a27b5e9e6248
unmyr <56982541+unmyr@users.noreply.github.com>
2023-10-31 06:25:34 +0900

    refactor: Rename a environment variable.

--- docker-examples/registry-noauth/test-docker-private-registry.sh
+++ docker-examples/registry-noauth/test-docker-private-registry.sh
@@ -3,7 +3,7 @@ NAMESPACE="my-private-registry-demo"
 PRIVATE_NS=""
 . ./.env.test
 test -z "${PRIVATE_NS}" && { echo "ERROR: A required environment variable is missing. : NAME='PRIVATE_NS'"; exit 1;}
-test -z "${MY_PRIVATE_REGISTRY}" && { echo "ERROR: A required environment variable is missing. : NAME='MY_PRIVATE_REGISTRY'"; exit 1;}
+test -z "${REGISTRY_FQDN_AND_PORT}" && { echo "ERROR: A required environment variable is missing. : NAME='REGISTRY_FQDN_AND_PORT'"; exit 1;}
 
 cat > test-docker-private-registry.yaml <<EOF
 apiVersion: v1
@@ -17,11 +17,11 @@ spec:
   - name: default
     image: ubuntu:22.04
     env:
-    - name: MY_PRIVATE_REGISTRY
+    - name: REGISTRY_FQDN_AND_PORT
       valueFrom:
         configMapKeyRef:
           name: private-registry
-          key: MY_PRIVATE_REGISTRY
+          key: REGISTRY_FQDN_AND_PORT
     volumeMounts:
     - name: ca-pem-store
       mountPath: /etc/ssl/certs/docker-registry.crt
@@ -64,14 +64,19 @@ create)
     kubectl wait -n "${NAMESPACE:-default}" --for=condition=Ready --timeout=60s pod/test-docker-private-registry
     kubectl get pods -n "${NAMESPACE:-default}"
     kubectl describe pods -n "${NAMESPACE:-default}"
-    kubectl exec -n "${NAMESPACE:-default}" -it pod/test-docker-private-registry -- curl https://${MY_PRIVATE_REGISTRY}/v2/_catalog
+    kubectl exec -n "${NAMESPACE:-default}" -it pod/test-docker-private-registry -- curl https://${REGISTRY_FQDN_AND_PORT}/v2/_catalog
     ;;
 
 delete)
     set -x
     kubectl delete -n "${NAMESPACE:-default}" -f test-docker-private-registry.yaml
-    kubectl delete -n "${NAMESPACE:-default}" configmap private-registry
-    kubectl delete -n "${NAMESPACE:-default}" configmap ca-pem-store
+    kubectl delete -n "${NAMESPACE:-default}" configmap ca-pem-store private-registry
     kubectl delete ns "${NAMESPACE:-default}"
     ;;
+
+show)
+    set -x
+    kubectl get -n "${NAMESPACE:-default}" all,configmap
+    ;;
+
 esac
