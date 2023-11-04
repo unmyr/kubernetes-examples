5c28fef3454059809cc384fa330b98df3a3b5d07
unmyr <56982541+unmyr@users.noreply.github.com>
2023-11-04 21:26:34 +0900

    docs(k8s_kustomize): Add kustomize examples.

---
+++ k8s-examples/kustomization/pod-env/deploy.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+usage() {
+    MESSAGE="$1"
+    test -n "${MESSAGE}" && echo "${MESSAGE}" 1>&2
+    cat <<EOF
+usage: $0 {preview|apply|delete|show}
+EOF
+}
+
+PART_OF_APP="kustomize-pod-demo"
+NAMESPACE="kustomize-pod-demo"
+
+SUB_COMMAND="$1"
+case "${SUB_COMMAND}" in
+preview)
+    set -x
+    kubectl kustomize ./
+    ;;
+
+apply)
+    set -x
+    kubectl create ns "${NAMESPACE:-default}" --dry-run=client -o yaml | kubectl apply -f -
+    kubectl kustomize ./ | kubectl apply -f -
+    kubectl get -n "${NAMESPACE:-default}" all
+    kubectl wait -n "${NAMESPACE:-default}" --for=condition=Ready pods --selector=app.kubernetes.io/part-of=${PART_OF_APP} --timeout=5s
+    set +x
+    kubectl get -n "${NAMESPACE:-default}" pods -o jsonpath="{range .items[*]}{.metadata.name}{'\n'}{end}" | while read POD_NAME; do
+        (set -x; kubectl get -n "${NAMESPACE:-default}" pod/${POD_NAME} -o jsonpath="{.spec.containers[0].env}{'\n'}")
+    done
+    ;;
+
+delete)
+    set -x
+    kubectl kustomize ./ | kubectl delete -f -
+    kubectl delete ns "${NAMESPACE:-default}"
+    ;;
+
+show)
+    set -x
+    kubectl get -n "${NAMESPACE:-default}" all
+    ;;
+
+*)
+    usage "ERROR: Unsupported subcommand : SUB-COMMAND='${SUB_COMMAND}'"
+    exit 1
+    ;;
+
+esac
