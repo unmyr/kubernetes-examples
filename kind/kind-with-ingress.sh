cc95cc760a64dbbb89a12087a8af1854954d520f
unmyr <56982541+unmyr@users.noreply.github.com>
2024-11-20 02:08:31 +0900

    feat(kind): Upgrade KIND from `v0.20.0` to `v0.25.0`.

--- kind/kind-with-ingress.sh
+++ kind/kind-with-ingress.sh
@@ -20,6 +20,19 @@ PV_NAME_POSTGRES="postgres-data-pv"
 
 SUB_COMMAND=$1
 case "${SUB_COMMAND}" in
+reinstall-go)
+    set -x
+    GO_VERSION_LATEST=$(git ls-remote --tag https://go.googlesource.com/go | awk '{print $2}' | sed -e '/\(beta[0-9]*\|rc[0-9]*\|release\|weekly\)/d; s@refs/tags/@@' | sort -V | tail -1)
+    test -f ${GO_VERSION_LATEST}.linux-amd64.tar.gz || {
+      curl -L -O https://go.dev/dl/${GO_VERSION_LATEST}.linux-amd64.tar.gz
+      sha256sum go*.tar.gz
+    }
+    sudo rm -rf ${GOROOT:-$HOME/go} ${GOPATH:-$HOME/go_projects}
+    tar -C $HOME -xzf ${GO_VERSION_LATEST}.linux-amd64.tar.gz
+    go version
+    go install sigs.k8s.io/kind@v0.25.0
+    ;;
+
 create)
     MOUNT_POINT_DOCKER_REGISTRY="/var/kind/docker-registry"
     MOUNT_POINT_POSTGRES="/tmp/kind-1-postgres-data"
@@ -124,7 +137,7 @@ spec:
             - ${KUBE_NODENAME_WORKER}
 EOF
     set -x
-    curl -L --silent -w '%{stdout}' https://raw.githubusercontent.com/projectcontour/contour/release-1.26/examples/render/contour.yaml | \
+    curl -L --silent -w '%{stdout}' https://raw.githubusercontent.com/projectcontour/contour/release-1.30/examples/render/contour.yaml | \
 sed -e 's/# \(cluster:\)/\1/' \
 -e '/#   configure the cluster dns lookup family/d' \
 -e '/#   valid options are: auto (default), v4, v6/d' \
@@ -138,7 +151,7 @@ kubectl apply -f -
     (set -x; kubectl rollout restart daemonset envoy -n projectcontour)
 
     : Install MetalLb
-    kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.10/config/manifests/metallb-native.yaml
+    kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.14.8/config/manifests/metallb-native.yaml
     kubectl rollout status deployment/controller -n metallb-system
     kubectl apply -f ${SCRIPT_DIR}/metallb-IPAddressPool.yaml
 
@@ -148,23 +161,34 @@ kubectl apply -f -
     kubectl patch -n kube-system deployment metrics-server --type=json -p '[{"op":"add","path":"/spec/template/spec/containers/0/args/-","value":"--kubelet-insecure-tls"}]'
 
     : Install cert-manager
-    (set -x; kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.12.0/cert-manager.yaml)
+    (set -x; kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.16.1/cert-manager.yaml)
     (set -x; kubectl get -n cert-manager deployment.apps,replicaset.apps,pods,service)
     (set -x; kubectl rollout status deployment -n cert-manager --timeout=90s)
     (set -x; kubectl get -n cert-manager deployment.apps,replicaset.apps,pods,service)
     : Install Service Binding
-    (set -x; kubectl apply -f https://github.com/servicebinding/runtime/releases/download/v0.4.0/servicebinding-runtime-v0.4.0.yaml)
+    (set -x; kubectl apply -f https://github.com/servicebinding/runtime/releases/download/v1.0.0/servicebinding-runtime-v1.0.0.yaml)
     ;;
 
 delete)
-    (set -x; kubectl delete -f https://github.com/servicebinding/runtime/releases/download/v0.4.0/servicebinding-runtime-v0.4.0.yaml)
+    (set -x; kubectl delete -f https://github.com/servicebinding/runtime/releases/download/v1.0.0/servicebinding-runtime-v1.0.0.yaml)
     (set -x; kubectl delete -f https://github.com/cert-manager/cert-manager/releases/download/v1.12.0/cert-manager.yaml)
     (set -x; kubectl delete -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml)
     (set -x; kubectl delete -f ${SCRIPT_DIR}/metallb-IPAddressPool.yaml)
-    (set -x; kubectl delete -f https://raw.githubusercontent.com/metallb/metallb/v0.13.10/config/manifests/metallb-native.yaml)
-    (set -x; kubectl delete -f https://raw.githubusercontent.com/projectcontour/contour/release-1.26/examples/render/contour.yaml)
-    (set -x; kubectl delete pvc -n postgres local-claim)
+    (set -x; kubectl delete -f https://raw.githubusercontent.com/metallb/metallb/v0.14.8/config/manifests/metallb-native.yaml)
+    (set -x; kubectl delete -f https://raw.githubusercontent.com/projectcontour/contour/release-1.30/examples/render/contour.yaml)
+    while read LINE; do
+        NS_AND_DEPLOY=($LINE)
+        DB_NAMESPACE=${NS_AND_DEPLOY[0]}
+        DB_DEPLOY_NAME=${NS_AND_DEPLOY[1]}
+        (
+          set -x
+          kubectl delete -n "${DB_NAMESPACE}" deploy,pvc,svc -l app=postgres
+        )
+    done< <(
+      kubectl get -A deploy -l app=${APP_NAME:-postgres} -o jsonpath='{range .items[*]}{.metadata.namespace}{"\t"}{.metadata.name}{"\n"}{end}'
+    )
     (set -x; kubectl delete persistentvolume ${PV_NAME_POSTGRES})
+
     (set -x; kubectl delete persistentvolume ${PV_NAME_DOCKER_REGISTRY})
     (set -x; kind delete cluster --name ${KIND_CLUSTER_NAME})
     ;;
